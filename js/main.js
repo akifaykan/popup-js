79c63f1c083f5cfe3bf82addb0f61feb4257a5e2
Akif Aykan <akifaykan@hotmail.com>
2021-10-18 16:39:56 +0300

    master

--- js/main.js
+++ js/main.js
@@ -1,25 +1,20 @@
-/*============== SHOW MODAL ===============*/
 const openBtn = document.getElementById('open-modal'),
-    modalFixed = document.getElementById('modal-fixed'),
     closeBtn = document.querySelectorAll('.close-modal'),
+    modalFixed = document.getElementById('modal-fixed'),
     modalContainer = document.getElementById('modal-container');
+    
+if (openBtn && modalContainer) {
+    openBtn.addEventListener('click', openModal);
+    closeBtn.forEach((c) => c.addEventListener('click', closeModal));
+    modalFixed.addEventListener('click', closeModal);
 
-const showModal = (openButton, modalContent) => {
-    if (openButton && modalContent) {
-        openButton.addEventListener('click', () => {
-            modalContent.classList.add('show-modal');
-            modalFixed.classList.add('open');
-        });
+    function openModal() {
+        modalContainer.classList.add('show-modal');
+        modalFixed.classList.add('open');
     }
-};
 
-showModal(openBtn, modalContainer);
-
-/*============== CLOSE MODAL ===============*/
-closeBtn.forEach((c) => c.addEventListener('click', closeModal));
-modalFixed.addEventListener('click', closeModal);
-
-function closeModal() {
-    modalContainer.classList.remove('show-modal');
-    modalFixed.classList.remove('open');
+    function closeModal() {
+        modalContainer.classList.remove('show-modal');
+        modalFixed.classList.remove('open');
+    }
 }
